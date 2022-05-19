276b8ff52b187a6de7d756c39f13958a03f14027
Alejandro Jesús Soto Mejías <s.alejandrojesus@contractor.temenos.com>
2022-05-19 13:40:21 +0200

    useLetters, useNumbers, useSymbols add  properties add

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,20 +3,47 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  _password = '';
+  private _useLetters: boolean = false;
+  private _useNumber: boolean = false;
+  private _useSymbol: boolean = false;
+  private _password: string = '';
 
   get password() {
-    return this._password; 
+    return this._password;
   }
 
   set password(value: string) {
-     this._password = value;
-   }
+    this._password = value;
+  }
 
-   onButtonClick = () => {
+  get useLetters() {
+    return this._useLetters;
+  }
+
+  set useLetters(value: boolean) {
+    this._useLetters = value;
+  }
+
+  get useNumbers() {
+    return this._useNumber;
+  }
+
+  set useNumbers(value: boolean) {
+    this._useNumber = value;
+  }
+
+  get useSymbols() {
+    return this._useSymbol;
+  }
+
+  set useSymbols(value: boolean) {
+    this._useSymbol = value;
+  }
+
+  onButtonClick = () => {
     this.password = 'Random String';
   };
 }
