7e066f118d47e60b38ba3a2d8ea740f23fb18abe
Alejandro Jesús Soto Mejías <s.alejandrojesus@contractor.temenos.com>
2022-05-19 17:03:07 +0200

    fix type problem

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,7 +10,7 @@ export class AppComponent {
   private _useNumbers: boolean = false;
   private _useSymbols: boolean = false;
   private _password: string = '';
-  _passwordLength: number = 0;
+  private _passwordLength: number = 0;
 
   get password() {
     return this._password;
@@ -44,8 +44,12 @@ export class AppComponent {
     this._useSymbols = value;
   }
 
-  onChangeLength(value: any) {
-    this._passwordLength = value.value;
+  get passwordLength() {
+    return this._passwordLength;
+  }
+
+  set passwordLength(event: number) {
+    this._passwordLength = event;
   }
 
   onButtonClick = () => {
