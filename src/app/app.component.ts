7612bfd3d5bd479083067245d5865970314f4998
Alejandro Soto <UTKON38@group.credem.net>
2022-06-14 15:16:48 +0200

    control generate button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -53,6 +53,26 @@ export class AppComponent {
   }
 
   onButtonClick = () => {
-    this.password = 'Random String';
+    const numbers: string = '1234567890';
+    const letters: string = 'abcdefghijklmnñopqrstuvwxyz';
+    const symbols: string = '!"·$%&/()=?¿';
+
+    let validChars: string = '';
+    if (this.useLetters) {
+      validChars += letters;
+    }
+    if (this.useNumbers) {
+      validChars += numbers;
+    }
+    if (this.useSymbols) {
+      validChars += symbols;
+    }
+
+    let generatedPassword: string = '';
+    for (let i = 0; i < this.passwordLength; i++) {
+      const index = Math.floor(Math.random() * validChars.length);
+      generatedPassword += validChars[index];
+    }
+    this.password = generatedPassword;
   };
 }
